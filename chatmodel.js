dad02b30dc2a933367ac6684af6056f588b4f55a
shalininagaraj085-ui <shalininagaraj085@gmail.com>
2026-09-18 01:46:49 +1100

    Update chatmodel.js
    Add chat schema

--- chatmodel.js
+++ chatmodel.js
@@ -1,1 +1,31 @@
+const mongoose = require("mongoose");
 
+const chatSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true
+    },
+
+    businessName: {
+      type: String,
+      required: true
+    },
+
+    message: {
+      type: String,
+      required: true
+    },
+
+    sender: {
+      type: String,
+      enum: ["user", "business"],
+      default: "user"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Chat", chatSchema);
