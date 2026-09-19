46f7f3c6add311a9034b80325bb915f206ff3a0b
shalininagaraj085-ui <shalininagaraj085@gmail.com>
2026-09-19 18:06:51 +1100

    Add live crowd status fields

--- businessmodel.js
+++ businessmodel.js
@@ -35,7 +35,29 @@ const businessSchema = new mongoose.Schema({
     closingTime: {
         type: String,
         default: "21:00"
+    },
+
+    liveStatus: {
+        type: String,
+        enum: ["Low Crowd", "Medium Crowd", "High Crowd", "Closed"],
+        default: "Low Crowd"
+    },
+
+    waitingTime: {
+        type: Number,
+        default: 0
+    },
+
+    statusNote: {
+        type: String,
+        default: ""
+    },
+
+    statusUpdatedAt: {
+        type: Date,
+        default: Date.now
     }
+
 });
 
 module.exports = mongoose.model("Business", businessSchema);
